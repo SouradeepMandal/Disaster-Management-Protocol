08d96dab7dd2daed4ff37c0c02b4024acd5f4d79
Souradeep Mandal <souradeepmandal459@gmail.com>
2026-07-25 07:39:12 +0000

    Add Hero Section to landing page

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero(){
+    return(
+        <section>
+            <h1>AI Disaster Management Platform</h1>
+            <p>Manage Disasters, rescue teams, and emergency response from one intelligent Platform</p>
+            <button>Report Incident</button>
+            <button>Explore Dashboard</button>
+        </section>
+    );
+}
+export default Hero;
